8f26c745e0d323ca60475c6b401af2abd9e8911d
Sophia Norris <snorris5@my.athens.edu>
2023-11-14 21:57:34 -0600

    q1

---
+++ apps/final.exam.q1/tree.hpp
@@ -0,0 +1,25 @@
+// Purpose of this program is to write three functions of the Tree class that provide information about a tree (hint traversal). The functions cannot be methods of the class
+
+#pragma once
+
+
+
+template <typename T>
+class Container 
+{
+	virtual size_t size() = 0;
+	virtual size_t max_size() = 0;
+	virtual bool empty() = 0;
+	virtual void swap(T&, T&) = 0;
+	virtual void clear() = 0;
+}
+
+
+
+
+for (list<string>::iterator j = projects.begin();
+	j != projects.end();
+	++j) {
+	cout << *j << endl;
+}
+return 0;
