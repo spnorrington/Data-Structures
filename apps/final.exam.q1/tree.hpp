34029f56d5bdbb928d669dc808e4ad4eb6276a16
Sophia Norris <snorris5@my.athens.edu>
2023-11-16 11:58:11 -0600

    cmake reamake config

--- apps/final.exam.q1/tree.hpp
+++ apps/final.exam.q1/tree.hpp
@@ -1,3 +1,4 @@
+// 
 // Purpose of this program is to write three functions of the Tree class that provide information about a tree (hint traversal). The functions cannot be methods of the class
 
 #pragma once
@@ -23,3 +24,36 @@ for (list<string>::iterator j = projects.begin();
 	cout << *j << endl;
 }
 return 0;
+
+//                        EXAMPLE DSFUI
+template <class Item>
+Item maximum(Item a, Item b)
+{
+	if (a > b) { return a; }
+	else { return b; }
+}
+int ival = 3;
+int ival2 = 4
+double dval = 2.55;
+double anotherDouble = 3.44;
+int maxint = maximum(ival, ival2);
+double maxdouble = maximum(dval, anotherDouble);
+
+//We aren’t limited to just a single type parameter in a template:
+:: :
+	Function templates can be overloaded
+	Each template must have a unique parameter list
+	template <class Item>
+
+template <class T>
+T sumAll(T num) ...
+template <class T1, class T2>
+T1 sumAll(T1 num1, T2 num2) ...
+
+template <class T>
+class Rectangle { ... };
+template <class T>
+class Square<T> : public Rectangle<T>
+{
+	...
+};
