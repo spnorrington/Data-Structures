8631a098c84ead5c0449be24132770e7dcb7f65b
Sophia Norris <snorris5@my.athens.edu>
2023-11-26 05:06:08 -0600

    reconfig

---
+++ apps/listtest/listtest.cpp
@@ -0,0 +1,55 @@
+//
+// This is a test file for the List class located in the header files directory
+// File:   listtest.cpp
+// Author: My Glorious Instructor <adam.lewis@athens.edu>
+// Purpose:
+// Demonstrate the use of our List class.
+//
+#include <iostream>
+#include "List.hpp"
+
+void printItem(int& item) {
+    std::cout << item << " ";
+}
+
+int main() {
+    List<int> aListOfIntegers;
+    if (aListOfIntegers.empty()) {
+        std::cout << "Yep, the new list is empty. " << std::endl;
+    }
+    for (int i = 3; i >= 0; i--) {
+        aListOfIntegers.push_front(i);
+    }
+    aListOfIntegers.traverse(printItem);
+    std::cout << std::endl;
+
+    std::cout << "Test iterators: Mutable -> ";
+    for (auto iter = aListOfIntegers.begin();
+        iter != aListOfIntegers.end();
+        ++iter) {
+        std::cout << *iter << " ";
+    }
+
+    std::cout << "Test iterators: const -> ";
+    for (auto iter = aListOfIntegers.cbegin();
+        iter != aListOfIntegers.cend();
+        ++iter) {
+        std::cout << *iter << " ";
+    }
+
+    std::cout << std::endl;
+    for (int i = 0; i < 3; i++) {
+        int datum = aListOfIntegers.back();
+        std::cout << datum << " ";
+        aListOfIntegers.pop_back();
+    }
+    std::cout << std::endl;
+    if (!aListOfIntegers.empty()) {
+        aListOfIntegers.traverse(printItem);
+        std::cout << std::endl;
+    }
+    aListOfIntegers.push_back(42);
+    aListOfIntegers.traverse(printItem);
+    std::cout << std::endl;
+    return 0;
+}
